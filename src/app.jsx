c025fe5bcf1295db4a42083c8f88bbecc3037dae
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-09-30 15:31:20 -0500

    Se agrego la estructura al proyecto

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+function TodoItem(){
+    return (
+        <li>
+            <span>V</span>
+            <p>Tomarse un litro de suavitel</p>
+            <span>X</span>
+        </li>
+    )
+}
+
+export default TodoItem
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import TodoItem from './app'
 
 const root = createRoot(document.getElementById('app'))
-root.render(<h1>Un saludo</h1>)
+root.render(<TodoItem />)
